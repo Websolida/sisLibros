17ea6c5a9304371dccda5bba4e1af5a6c41298da
Henry Pimentel <websolida@gmail.com>
2016-12-27 17:50:22 -0400

    Cargando datos desde el servidor

--- client/templates/posts/lista_posts.js
+++ client/templates/posts/lista_posts.js
@@ -1,18 +1,5 @@
-var postData = [
-    {
-        title: 'Introducion Telerecord',
-        url: 'http://www.google.com'
-    },
-    {
-        title: 'Diario Libre',
-        url: 'http://www.google.com'
-    },
-    {
-        title: 'Listin Diario',
-        url: 'http://www.google.com'
-    }
-];
-
 Template.listaPosts.helpers({
-   posts: postData
+   posts: function () {
+       return Posts.find();
+   }
 });
---
+++ server/fixtures.js
@@ -0,0 +1,16 @@
+if(Posts.find().count()=== 0){
+    Posts.insert({
+        title: 'Introducion a las IT',
+        url: 'http://www.google.com'
+    });
+
+    Posts.insert({
+        title: 'Desarrollo de las IT',
+        url: 'http://www.google.com'
+    });
+
+    Posts.insert({
+        title: 'Conclusiones de las IT',
+        url: 'http://www.google.com'
+    });
+}
